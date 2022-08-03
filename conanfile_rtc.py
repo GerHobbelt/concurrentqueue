d4c4d7a483e1ab1c25b7e687b4b0cae086d07916
Innes MacKenzie <imackenzie@esri.com>
2022-08-03 13:43:08 +0100

    Add version, and fix exports_sources

--- conanfile_rtc.py
+++ conanfile_rtc.py
@@ -5,6 +5,7 @@ from conans import ConanFile
 
 class ConcurrentQueueConan(ConanFile):
     name = "ConcurrentQueue"
+    version = "1.0.3"
     description = " A fast multi-producer, multi-consumer lock-free concurrent queue "
     topics = ("conan," "ConcurrentQueue", "lockfree", "queue")
     url = "https://github.com/cameron314/concurrentqueue"
@@ -12,12 +13,12 @@ class ConcurrentQueueConan(ConanFile):
     author = "Cameron <cameron@moodycamel.com>"
     license = "Simplified BSD"
     exports = ["LICENSE.md"]
-    exports_sources = ["src/*"]
+    exports_sources = ["*"]
     no_copy_source = True
 
     def package(self):
         self.copy(pattern="LICENSE.md", dst="licenses")
-        self.copy(pattern="*", dst="include", src=".", keep_path=True)
+        self.copy(pattern="concurrentqueue.h", dst="include/concurrentqueue", src=".", keep_path=True)
 
     def package_info(self):
         if not self.in_local_cache:
