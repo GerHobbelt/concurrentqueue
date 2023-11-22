15521a1acb9f2dbe27088aa9fb445a16949e5521
Ger Hobbelt <ger@hobbelt.com>
2023-11-22 11:12:24 +0100

    ditch those pesky "#pragma comment(lib, XYZ)" statements where some folks are being "helpful" by injecting these statements to help load/link their own libraries. Only leave these when they're about operating system libraries)

--- benchmarks/tbb/internal/_tbb_windef.h
+++ benchmarks/tbb/internal/_tbb_windef.h
@@ -54,19 +54,19 @@ namespace std {
 #    endif
 #endif
 
-#if (__TBB_BUILD || __TBBMALLOC_BUILD) && !defined(__TBB_NO_IMPLICIT_LINKAGE)
+#if (__TBB_BUILD || __TBBMALLOC_BUILD || defined(BUILD_MONOLITHIC)) && !defined(__TBB_NO_IMPLICIT_LINKAGE)
 #define __TBB_NO_IMPLICIT_LINKAGE 1
 #endif
 
 #if _MSC_VER
     #if !__TBB_NO_IMPLICIT_LINKAGE
         #ifdef __TBB_LIB_NAME
-	        #pragma comment(lib, __TBB_STRING(__TBB_LIB_NAME))
+	        //#pragma comment(lib, __TBB_STRING(__TBB_LIB_NAME))
         #else
 			#ifdef _DEBUG
-				#pragma comment(lib, "tbb_debug.lib")
+				//#pragma comment(lib, "tbb_debug.lib")
 			#else
-				#pragma comment(lib, "tbb.lib")
+				//#pragma comment(lib, "tbb.lib")
 			#endif
         #endif
     #endif
